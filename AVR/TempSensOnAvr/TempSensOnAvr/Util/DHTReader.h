d6b84e4cf3be26902982b56ddd0e10829d413103
Valerij Bielskij <Valerij.Bielskij.contractor@draeger.com>
2019-03-16 15:51:52 +0200

    Fixed formating in AVR project

--- AVR/TempSensOnAvr/TempSensOnAvr/Util/DHTReader.h
+++ AVR/TempSensOnAvr/TempSensOnAvr/Util/DHTReader.h
@@ -13,70 +13,70 @@
 class DHTReader
 {
 public:
-	DHTReader(volatile uint8_t* directionReg, volatile uint8_t* portReg, volatile uint8_t* pinReg, uint8_t pin) : m_directionReg(directionReg), m_portReg(portReg), m_pinReg(pinReg), m_pin(pin) {}
-	~DHTReader() = default;
+    DHTReader(volatile uint8_t* directionReg, volatile uint8_t* portReg, volatile uint8_t* pinReg, uint8_t pin) : m_directionReg(directionReg), m_portReg(portReg), m_pinReg(pinReg), m_pin(pin) {}
+    ~DHTReader() = default;
 
-	DHTData getData()
-	{
-		//output && low
-		bitSet(*m_directionReg, m_pin);
-		bitClear(*m_portReg, m_pin);
+    DHTData getData()
+    {
+        //output && low
+        bitSet(*m_directionReg, m_pin);
+        bitClear(*m_portReg, m_pin);
 		
-		_delay_ms(20);
+        _delay_ms(20);
 
-		//set input && pullup
-		bitClear(*m_directionReg, m_pin);
-		bitSet(*m_portReg, m_pin);
-		
-		auto pulseLength = pulseIn(&PINB, 0, LOW, 100);
+        //set input && pullup
+        bitClear(*m_directionReg, m_pin);
+        bitSet(*m_portReg, m_pin);
 
-		if(pulseLength < 75)
-		{
-			return DHTData{DHTData::Status::TimeoutOnSync1};
-		}
+        auto pulseLength = pulseIn(&PINB, 0, LOW, 100);
 
-		if(!waitForState(m_pinReg, m_pin, LOW, 5000))
-		{
-			return DHTData{DHTData::Status::TimeoutOnSync2};
-		}
+        if(pulseLength < 75)
+        {
+            return DHTData{DHTData::Status::TimeoutOnSync1};
+        }
 
-		byte data[5] = {0};
-		byte count = 0;
-		
-		while(count < 40)
-		{
-			pulseLength = pulseIn(m_pinReg, m_pin, HIGH, 140);
+        if(!waitForState(m_pinReg, m_pin, LOW, 5000))
+        {
+            return DHTData{DHTData::Status::TimeoutOnSync2};
+        }
 
-			if(pulseLength == 0)
-			{
-				return DHTData{DHTData::Status::TimeoutOnDataReceive};
-			}
+        byte data[5] = {0};
+        byte count = 0;
 
-			if(pulseLength > 60)
-			{
-				const auto index = count / BITS_IN_BYTE;
-				const auto bit = BITS_IN_BYTE - 1 - count % BITS_IN_BYTE; //reverse order
-				bitSet(data[index], bit);
-			}
+        while(count < 40)
+        {
+            pulseLength = pulseIn(m_pinReg, m_pin, HIGH, 140);
 
-			count++;
-		}
+            if(pulseLength == 0)
+            {
+                return DHTData{DHTData::Status::TimeoutOnDataReceive};
+            }
 
-		const byte crc = data[0] + data[1] + data[2] + data[3];
+            if(pulseLength > 60)
+            {
+                const auto index = count / BITS_IN_BYTE;
+                const auto bit = BITS_IN_BYTE - 1 - count % BITS_IN_BYTE; //reverse order
+                bitSet(data[index], bit);
+            }
 
-		if(crc != data[4])
-		{
-			return DHTData{DHTData::Status::ChecksumFailed};
-		}
+            count++;
+        }
 
-		return DHTData{toFloat(data[2], data[3]), toFloat(data[0], data[1])};
-	}
+        const byte crc = data[0] + data[1] + data[2] + data[3];
+
+        if(crc != data[4])
+        {
+            return DHTData{DHTData::Status::ChecksumFailed};
+        }
+
+        return DHTData{toFloat(data[2], data[3]), toFloat(data[0], data[1])};
+    }
 	
 private:
-	volatile uint8_t* const m_directionReg;
-	volatile uint8_t* const m_portReg;
-	volatile uint8_t* const m_pinReg;
-	const uint8_t m_pin;
+    volatile uint8_t* const m_directionReg;
+    volatile uint8_t* const m_portReg;
+    volatile uint8_t* const m_pinReg;
+    const uint8_t m_pin;
 };
 
 #endif /* DHT_READER_H */
--- AVR/TempSensOnAvr/TempSensOnAvr/Util/Timer.h
+++ AVR/TempSensOnAvr/TempSensOnAvr/Util/Timer.h
@@ -16,86 +16,86 @@ volatile unsigned long overflowCount = 0; //stores total count of overflows
 
 void clock_init()
 {
-	//set timer2 normal mode
-	TCCR2A = 0;
+    //set timer2 normal mode
+    TCCR2A = 0;
 	
-	//enable prescaler of 64
-	TCCR2B |= (1 << CS22);
+    //enable prescaler of 64
+    TCCR2B |= (1 << CS22);
 
-	//clear timer counter
-	TCNT2 = 0;
+    //clear timer counter
+    TCNT2 = 0;
 
-	//enable overflow interrupt
-	TIMSK2 |= (1 << TOIE2);
+    //enable overflow interrupt
+    TIMSK2 |= (1 << TOIE2);
 
-	//enable global interrupts
-	sei();
+    //enable global interrupts
+    sei();
 }
 
 ISR(TIMER2_OVF_vect)
 {
-	//interrupt routine gets called on every timer overflow
-	overflowCount++;
+    //interrupt routine gets called on every timer overflow
+    overflowCount++;
 }
 
 unsigned long micros()
 {
-	byte oldSREG = SREG; //save register
+    byte oldSREG = SREG; //save register
 	
-	cli(); //disable interrupts
+    cli(); //disable interrupts
 	
-	auto overflows = overflowCount;
-	auto ticks = TCNT2;
+    auto overflows = overflowCount;
+    auto ticks = TCNT2;
 
-	//extra check if overflow already happened but not yet interrupted
-	if ((TIFR2 & (1 << TOV2)) && (ticks < 255))
-	{
-		overflows++;
-	}
+    //extra check if overflow already happened but not yet interrupted
+    if ((TIFR2 & (1 << TOV2)) && (ticks < 255))
+    {
+    	overflows++;
+    }
 
-	//restore previous interrupt enabled state
-	SREG = oldSREG;
+    //restore previous interrupt enabled state
+    SREG = oldSREG;
 
-	//total overflows + counting ticks by cycle rate in microseconds
-	return (overflows * 256 + ticks) * CLOCK_CYCLES_PER_US;
+    //total overflows + counting ticks by cycle rate in microseconds
+    return (overflows * 256 + ticks) * CLOCK_CYCLES_PER_US;
 }
 
 bool waitForState(volatile uint8_t* gpio, uint16_t pin, byte state, unsigned int timeout, unsigned long start = micros())
 {
-	while(getBit(*gpio, pin) != state)
+    while(getBit(*gpio, pin) != state)
+    {
+    	auto diff = micros() - start;
+	if(diff >= timeout)
 	{
-		auto diff = micros() - start;
-		if(diff >= timeout)
-		{
-			return false;
-		}
+            return false;
 	}
+     }
 
-	return true;
+     return true;
 }
 
 long pulseIn(volatile uint8_t* gpio, uint16_t pin, byte state, unsigned int timeout)
 {
-	auto start = micros();
+    auto start = micros();
 
-	if(!waitForState(gpio, pin, !state, timeout, start))
-	{ //wait till expected state is set
-		return 0;
-	}
+    if(!waitForState(gpio, pin, !state, timeout, start))
+    { //wait till initial state is set
+ 	return 0;
+    }
 
-	if(!waitForState(gpio, pin, state, timeout, start))
-	{ //wait for transition HIGH -> LOW or LOW -> HIGH
-		return 0;
-	}
+    if(!waitForState(gpio, pin, state, timeout, start))
+    { //wait for transition HIGH -> LOW or LOW -> HIGH
+	return 0;
+    }
 
-	auto end = micros();
+    auto end = micros();
 
-	if(!waitForState(gpio, pin, !state, timeout, start))
-	{ //wait for transition LOW -> HIGH or HIGH -> LOW
-		return 0;
-	}
+    if(!waitForState(gpio, pin, !state, timeout, start))
+    { //wait for transition LOW -> HIGH or HIGH -> LOW
+  	return 0;
+    }
 
-	return micros() - end;
+    return micros() - end;
 }
 
 #endif /* TIMER_H */
